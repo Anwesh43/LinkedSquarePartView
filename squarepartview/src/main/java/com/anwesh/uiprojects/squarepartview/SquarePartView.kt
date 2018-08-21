062db42241c384d0ec6ca174199ffae1d516a94d
Anwesh43 <anweshthecool0@gmail.com>
2018-08-21 09:28:02 +0530

    adding SquarePartView class

--- squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
+++ squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
@@ -11,3 +11,20 @@ import android.content.Context
 
 val nodes : Int = 4
 
+class SquarePartView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
