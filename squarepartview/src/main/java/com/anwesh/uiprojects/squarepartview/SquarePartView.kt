52313e4c5d3ef93202bdc01fd432079230d67e12
Anwesh43 <anweshthecool0@gmail.com>
2018-08-21 09:54:42 +0530

    adding LinkedSquarePart class

--- squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
+++ squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
@@ -132,4 +132,27 @@ class SquarePartView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedSquarePart(var i : Int) {
+
+        private var curr : SquarePartNode = SquarePartNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
