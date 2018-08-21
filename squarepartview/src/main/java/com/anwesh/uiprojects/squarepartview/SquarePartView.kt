f7ebead022932347019d7256422a18aad1adcd18
Anwesh43 <anweshthecool0@gmail.com>
2018-08-21 09:50:32 +0530

    adding SquarePartNode class

--- squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
+++ squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
@@ -89,4 +89,47 @@ class SquarePartView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SquarePartNode(var i : Int, val state : State = State()) {
+        private var next : SquarePartNode? = null
+        private var prev : SquarePartNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes -1) {
+                next = SquarePartNode(i)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSquarePartNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SquarePartNode {
+            var curr : SquarePartNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
