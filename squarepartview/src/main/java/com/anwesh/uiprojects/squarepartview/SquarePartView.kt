075307126de85b7cf937a5dc6a937da14f1eefd2
Anwesh43 <anweshthecool0@gmail.com>
2018-08-21 09:26:07 +0530

    importing libraries

--- squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
+++ squarepartview/src/main/java/com/anwesh/uiprojects/squarepartview/SquarePartView.kt
@@ -3,4 +3,11 @@ package com.anwesh.uiprojects.squarepartview
 /**
  * Created by anweshmishra on 21/08/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 4
 
