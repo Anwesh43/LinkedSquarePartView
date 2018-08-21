60e947e394d56fb134582e73faad901b5fd9238d
Anwesh43 <anweshthecool0@gmail.com>
2018-08-21 10:33:44 +0530

    adding SquarePartView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarepartview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarepartview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarepartview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarepartview.SquarePartView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquarePartView.create(this)
     }
 }
